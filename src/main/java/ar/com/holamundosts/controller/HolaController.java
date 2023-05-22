706b63a88da8a66e896890a8ab87be8537455d5c
Martin Vega <martin.vega@osde.com.ar>
2023-05-21 21:37:25 -0300

    Commit inicial

---
+++ src/main/java/ar/com/holamundosts/controller/HolaController.java
@@ -0,0 +1,54 @@
+package ar.com.holamundosts.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HolaController {
+	
+	
+	@Value("${ambiente}")
+	private String ambiente;
+
+	
+	
+	@GetMapping("/saludar")
+	public String saludar() {
+		return "Hola mundo Spring Boot";
+	}
+	
+	@GetMapping("/saludo1/{variable}")
+	public String saludo1(@PathVariable(value = "variable") String variable) {
+		return "Hola "+variable;
+	}
+	
+	@GetMapping("/saludo2")
+	public String saludo2(@PathParam(value = "nombre") String nombre) {
+		return "Hola "+nombre+" saludando con @PathParam";
+	}
+	
+	@GetMapping("/verambiente")
+	public String verAmbiente() {
+		return "Estoy en el ambiente:" +ambiente;
+	}
+	
+	
+
+	public String getAmbiente() {
+		return ambiente;
+	}
+
+	public void setAmbiente(String ambiente) {
+		this.ambiente = ambiente;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/ar/com/holamundosts/HolamundostsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.holamundosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
